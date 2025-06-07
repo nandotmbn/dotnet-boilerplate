187ec285c9b18991fa35abb8adf29f9efa4e16ae
nandotmbn <orlandopratama695@gmail.com>
2025-06-07 23:26:07 +0700

    feat: production setting

--- 1. Databases/init.sh
+++ 1. Databases/init.sh
@@ -3,34 +3,45 @@
 # Function to create a database
 create_database() {
   local dbname=$1
+  local ctname=$2
   echo "Creating database $dbname..."
-  docker exec databases-psql-1 psql -U postgres -p $PORT -c "CREATE DATABASE $dbname"
+  docker exec $ctname psql -U postgres -p $PORT -c "CREATE DATABASE $dbname"
 }
 
 # Function to grant privileges on a database
 grant_privileges() {
   local dbname=$1
+  local ctname=$2
   echo "Granting privileges on database $dbname..."
-  docker exec databases-psql-1 psql -U postgres -p $PORT -c "GRANT ALL PRIVILEGES ON DATABASE $dbname TO gasspolldatabasejoss;"
+  docker exec $ctname psql -U postgres -p $PORT -c "GRANT ALL PRIVILEGES ON DATABASE $dbname TO user;"
 }
 
 # Function to grant privileges on the public schema
 grant_schema_privileges() {
   local dbname=$1
+  local ctname=$2
   echo "Granting schema privileges on database $dbname..."
-  docker exec databases-psql-1 psql -U postgres -d $dbname -p $PORT -c "GRANT ALL ON SCHEMA public TO gasspolldatabasejoss;"
+  docker exec $ctname psql -U postgres -d $dbname -p $PORT -c "GRANT ALL ON SCHEMA public TO user;"
 }
 
 # Main function to set up a database
 setup_database() {
   local dbname=$1
-  create_database $dbname
-  grant_privileges $dbname
-  grant_schema_privileges $dbname
+  local ctname=$2
+  create_database $dbname $ctname
+  grant_privileges $dbname $ctname
+  grant_schema_privileges $dbname $ctname
 }
 
-# Environment variable for port (set this according to your setup)
-PORT=21601
-
 # Set up the databases
-setup_database database
+
+PORT=5432
+setup_database database 1databases-psql-1
+
+setup_database database 1databases-psql1-1
+
+setup_database database 1databases-psql2-1
+
+setup_database database 1databases-psql3-1
+
+setup_database database 1databases-psql4-1
